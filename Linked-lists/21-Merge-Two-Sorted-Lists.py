ed2f68e09fc4396cd6166cedc0d11acc53d7f287
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-09-18 23:39:15 +0530

    [21] [Time Beats: 64.83%] [Memory Beats: 97.35%] - LeetPush

--- Linked-lists/21-Merge-Two-Sorted-Lists.py
+++ Linked-lists/21-Merge-Two-Sorted-Lists.py
@@ -5,22 +5,16 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        temp1=list1
-        temp2=list2
-        dummy=ListNode()
-        cur=dummy
-        while temp1 is not None and temp2 is not None:
-            if temp1.val<=temp2.val:
-                cur.next=temp1
-                cur=cur.next
-                temp1=temp1.next
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
             else:
-                cur.next=temp2
-                cur=cur.next
-                temp2=temp2.next
-        if temp1 is not None:
-            cur.next=temp1
-        else:
-            cur.next=temp2
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
         return dummy.next
-        
