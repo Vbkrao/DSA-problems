ee30324267328234cab7cf19fae4eec57fe1f06b
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-09-24 13:31:20 +0530

    [53] [Time Beats: 32.50%] [Memory Beats: 71.04%] - LeetPush

--- Arrays/53-Maximum-Subarray.py
+++ Arrays/53-Maximum-Subarray.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum=float('-inf')
-        currentSum=0
-        for num in nums:
-            currentSum+=num
-            if currentSum>maxSum:
-                maxSum=currentSum
-            if currentSum<0:
-                currentSum=0
+        #Kadane's Algo:O(n)
+        maxSum=nums[0]
+        curSum=0
+        for i in nums:
+            curSum=max(curSum,0)
+            curSum+=i
+            maxSum=max(maxSum,curSum)
         return maxSum
+
         
