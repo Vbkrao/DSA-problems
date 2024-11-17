c458204aca4d95af9c40324b0a0dd43b5ee7d86c
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-11-18 00:00:54 +0530

    [2425] [Time Beats: 57.94%] [Memory Beats: 46.93%] - LeetPush

--- Arrays/2425-Bitwise-XOR-of-All-Pairings.py
+++ Arrays/2425-Bitwise-XOR-of-All-Pairings.py
@@ -1,7 +1,13 @@
-from functools import reduce
-from operator import xor
-
-
 class Solution:
-    def xorAllNums(self, a: List[int], b: List[int]) -> int:
-        return reduce(xor, a * (len(b) & 1) + b * (len(a) & 1), 0)
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        n1, n2 = len(nums1), len(nums2)
+        ans = 0
+        if n2%2==1:
+            for i in range(n1):
+                ans = ans ^ nums1[i]
+        
+        if n1%2==1:
+            for i in range(n2):
+                ans = ans ^ nums2[i]
+        
+        return ans
