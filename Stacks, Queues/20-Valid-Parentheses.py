c3d9d778b938350590a30ed8ee263857fa6f25eb
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-12-31 13:22:37 +0530

    [20] [Time Beats: 100.00%] [Memory Beats: 6.37%] - LeetPush

--- Stacks, Queues/20-Valid-Parentheses.py
+++ Stacks, Queues/20-Valid-Parentheses.py
@@ -1,17 +1,25 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stk = []
-        for char in s:
-            if char in '({[':
-                stk.append(char)
-            else:
-                if not stk:
-                    return False
+        # for char in s:
+        #     if char in '({[':
+        #         stk.append(char)
+        #     else:
+        #         if not stk:
+        #             return False
 
-                x = stk.pop()
-                if (x == '(' and char == ')') or (x == '{' and char == '}') or (x == '[' and char == ']'):
-                    continue
-                else:
+        #         x = stk.pop()
+        #         if (x == '(' and char == ')') or (x == '{' and char == '}') or (x == '[' and char == ']'):
+        #             continue
+        #         else:
+        #             return False
+        # return not stk
+        mapping={\)\:\(\,\}\:\{\,\]\:\[\}
+        for char in s:
+            if char in mapping.values():
+                stk.append(char)
+            elif char in mapping.keys():
+                if not stk or mapping[char]!=stk.pop():
                     return False
         return not stk
             
